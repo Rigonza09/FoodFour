6eeaac336405c7259b2acc70a4d8897a693f4bd7
RVMA <rvma.182@gmail.com>
2017-11-19 02:04:25 -0700

    le puse algo
    le puse logo o algo asi

--- FoodFour FINAL/FoodFour FINAL/obj/Debug/android/foodfour_final/foodfour_final/R.java
+++ FoodFour FINAL/FoodFour FINAL/obj/Debug/android/foodfour_final/foodfour_final/R.java
@@ -10,11 +10,14 @@ package foodfour_final.foodfour_final;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int logo_final=0x7f020000;
+    }
     public static final class layout {
-        public static final int login=0x7f020000;
-        public static final int main=0x7f020001;
+        public static final int login=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
 }
