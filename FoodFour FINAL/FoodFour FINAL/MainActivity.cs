2b3a0fa098b5f82a09026129f46fbbb124b90d30
Rigoberto Duarte Gonzalí <rigodg00@gmail.com>
2017-11-19 17:39:37 -0700

    Cambios simples
    Se cambiaron los margenes del layout de login, ademas se referencia a esa pantalla dentro de main, asi como se hizo lock a la orientación de la pantalla --YA NO SE PUEDE UTILIZAR DE MANERA VERTICAL, SOLO HORIZONTAL (LANDSCAPE)--

--- FoodFour FINAL/FoodFour FINAL/MainActivity.cs
+++ FoodFour FINAL/FoodFour FINAL/MainActivity.cs
@@ -4,7 +4,7 @@ using Android.OS;
 
 namespace FoodFour_FINAL
 {
-    [Activity(Label = "FoodFour_FINAL", MainLauncher = true)]
+    [Activity(Label="FoodFour",MainLauncher = true,Icon ="@drawable/logo_icon",ScreenOrientation =Android.Content.PM.ScreenOrientation.SensorLandscape)]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -12,7 +12,7 @@ namespace FoodFour_FINAL
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.login);
         }
     }
 }
